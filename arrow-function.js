b8394b11f03fab4eb835130b34ba3a152a993df2
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-24 18:51:46 +0600

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,83 @@
+// function declaration     
+
+function arrow(first, second) {
+
+    const sumation = first + second;
+
+    return sumation;
+
+}
+
+const result = arrow(10, 20);
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+// function declaration difference function expression 
+
+// function declaration 
+const number = 2;
+
+
+// function expression 
+const numberfunction = function adda(first, second) {
+    const total = first + second;
+    return total;
+}
+
+
+
+// function expression with annonimous
+const anouthernumberfunction = function (first, second) {
+    const total = first + second;
+    return total;
+}
+
+
+
+
+
+
+
+
+
+
+//this is normal function 
+function add4(first, second) {
+    return first + second;
+}
+const add5 = function (first, second) {
+    return first + second;
+}
+
+
+
+
+
+
+
+
+
+// this is arrow function wow 
+const add6 = (first, second) => first + second;
+const totalResult = add6(10, 20);
+console.log(totalResult);
+
+
+
+
+
+// interview question
+// difference between regular function vs declaration function vs regular expression vs arrow function 
+
+
+
